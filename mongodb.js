208578306c2031aea8abea22e49c8cb434c7bd21
Fox112345 <Fox112345@yandex.ru>
2019-12-04 17:07:24 +0300

    add connect 2

--- mongodb.js
+++ mongodb.js
@@ -1,15 +1,27 @@
 // CRUD create read update delete
 const mongodb = require('mongodb')
-const MongoClient  = mongodb.MongoClient
+const MongoClient = mongodb.MongoClient
 
 const connectionURL = 'mongodb://localhost:27017';
 const databaseName = 'task-manager';
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
+MongoClient.connect(connectionURL, {
+    useNewUrlParser: true
+}, (err, client) => {
     if (err) {
         return console.log("Unable to connect to DB");
     }
 
-    console.log("Connected correctly");
+    const db = client.db(databaseName);
+    db.collection('users').insertOne({
+        name: 'Me',
+        age: 12
+    }, (err , result) => {
+        if (err) console.log(err);
+        console.log(result.opt);
+    })
+
     
+    
+
 })
