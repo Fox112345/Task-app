244aaa4cdebff204b56b48109e7ca68b0133a014
Fox112345 <Fox112345@yandex.ru>
2019-12-24 21:03:09 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express =  require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/tasks')
+
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,3 +14,5 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is runing on port ' + port);
 })
+
+const jwt = require('jsonwebtoken');
--- src/routers/user.js
+++ src/routers/user.js
@@ -15,6 +15,16 @@ router.post('/users', async (req, res) => {
 
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
 router.get('/users', async (req, res) => {
 
     try {
@@ -48,10 +58,16 @@ router.patch('/users/:id', async (req, res) => {
     })
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+        const user = await User.findById(req.params.id);
+
+        updates.forEach( update => user[update] = req.body[update])
+
+        await user.save()
+
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        //     new: true,
+        //     runValidators: true
+        // });
 
         if (!user) return res.status(404).send()
         res.send(user)
@@ -64,7 +80,6 @@ router.patch('/users/:id', async (req, res) => {
 router.delete('/users/:id', async (req, res) => {
 
     try {
-
         const user = await User.findByIdAndRemove(req.params.id)
         if (!user) return res.status(404).send()
         res.send(user)
